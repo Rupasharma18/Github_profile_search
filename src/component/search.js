e6c0f865500d98771bd53a9ff999a42c3a4261bd
Rupasharma18 <rupa18@navgurukul.org>
2020-06-27 23:45:47 +0530

    input value change

--- src/component/search.js
+++ src/component/search.js
@@ -1,31 +1,40 @@
 import React, { useState } from 'react';
-import { FormGroup, Form, InputGroup,Input, InputGroupAddon, Button } from 'reactstrap';
+import { FormGroup, Form, InputGroup, Input, InputGroupAddon, Button } from 'reactstrap';
 
-const Search =({searchUser})=>{
-    const [query, setQuery]=useState('')
-    const useInput =(e)=>{
-       setQuery(e.target.value)
+const Search = ({ searchUser }) => {
+    const [query, setQuery] = useState('')
+
+    const useInput = (e) => {
+        const regex = /[A-Za-z]/
+        const re = /^[0-9\b]+$/;
+        if ((e.target.value === '' || regex.test(e.target.value)) || (e.target.value === '' || re.test(e.target.value))) {
+            setQuery(e.target.value)
+        }
+        else{
+            alert('nhi hai')
+        }
+       
     }
- 
-    return(
-      <Form className='mt-5 mb-5' 
-       onSubmit={(e)=>
-        {e.preventDefault();
-        searchUser(query);
-      }}>
-          <FormGroup>
-              <InputGroup>
-              <Input placeholder="search github id..."
-              onChange={useInput}
-              value={query}
-             />
-            
-              <InputGroupAddon addonType='append'>
-                  <Button type='submit'disabled={!query} >search</Button>
-              </InputGroupAddon>
-              </InputGroup>
-          </FormGroup>
-      </Form>
+
+    return (
+        <Form className='mt-5 mb-5'
+            onSubmit={(e) => {
+                e.preventDefault();
+                searchUser(query);
+            }}>
+            <FormGroup>
+                <InputGroup>
+                    <Input placeholder="search github id..."
+                        onChange={useInput}
+                        value={query}
+                    />
+
+                    <InputGroupAddon addonType='append'>
+                        <Button type='submit' disabled={!query} >search</Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+        </Form>
     )
 }
 export default Search;
